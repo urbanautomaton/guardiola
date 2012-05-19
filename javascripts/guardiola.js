584a66e3157767b3457e411d3224a0691fb20b52
Simon Coffey <simon@tribesports.com>
2012-05-19 22:17:02 +0100

    More homunculi

--- javascripts/guardiola.js
+++ javascripts/guardiola.js
@@ -14,7 +14,7 @@ Array.prototype.shuffle = function() {
 
 Guardiola.data = {
   intro: ["Don't be ridiculous", "Of course not", "You're off your nut", "Get to fuck", "Stop taking the piss", "Are you taking the piss? Sheesh", "For fuck's sake", "Oh, piss off", "Get a fucking grip"],
-  insult: ["chump", "suppurating kidney", "charlie", "turnip", "unpleasant comestible", "hamtoucher", "player of the pink oboe", "fan of Paul McCartney"],
+  insult: ["chump", "suppurating kidney", "charlie", "turnip", "unpleasant comestible", "hamtoucher", "player of the pink oboe", "fan of Paul McCartney", "execrable human", "ruin", "homunculus"],
   verb: ["revolutionising", "developing a new technique for", "rethinking", "turning his hand to", "reinventing", "demolishing the world of", "winning at"],
   occupation: ["fish husbandry", "gregorian plainchant", "molecular gastronomy", "the lyric poem", "french knitting", "Klingon opera", "timber technology"],
   place: ["in Botswana", "in a small monastery outside Brive", "on a dirigible circling the Kremlin", "in a walk-in wardrobe that only opens on Thursdays", "in a hand-built reproduction Ford Cortina", "on the Kazakh/Kyrgyz border"]
